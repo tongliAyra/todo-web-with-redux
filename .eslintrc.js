c29e0af3eb24f5abd2fb089ea7c19962a1538cb1
tongliAyra <tong.li@thoughtworks.com>
2022-10-26 14:35:18 +0800

    feat: add eslint cli to fix format

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,11 @@ module.exports = {
   plugins: [
     'react'
   ],
+  settings: {
+    'react': {
+      'version': 'detect'
+    }
+  },
   rules: {
     'indent': ['error', 2],
     'linebreak-style': ['error', 'unix'],
