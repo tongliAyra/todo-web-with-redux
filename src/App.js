4a0b0ff1fcf842dd549205a1bacd4c5dd8928153
tongliAyra <tong.li@thoughtworks.com>
2022-10-27 09:15:35 +0800

    fix: fix eslint config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import React from 'react'
 import { Header } from './components/header/Header'
 import { TodoList } from './components/todo-list/TodoList'
 import { CompletedList } from './components/completed-list/CompletedList'
-import React from 'react'
 
 export const App = () => {
   return (
