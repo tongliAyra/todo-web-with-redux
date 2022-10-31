4cbd1895747c69eface847d08ff3d5189b364ea5
tongliAyra <tong.li@thoughtworks.com>
2022-11-01 00:27:27 +0800

    fix: delete unused component

--- src/components/error-message/ErrorMessage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './style.css'
-
-const ERROR_MESSAGE = 'Please input something first!'
-export const ErrorMessage = () => {
-  return <p className="error">{ ERROR_MESSAGE }</p>
-}
